89ba3c4ba3e5ba3eba97b952d3ae455e07bc3af0
Brian <s4.brianvu@wearesection.com>
2022-05-18 07:30:15 +0700

    [Add]_React 2022-05-18

--- my-app/src/components/news/HackerNews.js
+++ my-app/src/components/news/HackerNews.js
@@ -3,7 +3,7 @@ import React, { useRef, useState } from "react";
 import lodash from "lodash";
 const HackerNews = () => {
   const [hits, setHits] = useState([]);
-  const [query, setQuery] = useState("react");
+  const [query, setQuery] = useState("");
   const [loading, setLoading] = useState(true);
   const [errorMessage, setErrorMessage] = useState("");
 
@@ -22,29 +22,34 @@ const HackerNews = () => {
     }
   };
 
-  const handleUpdateValue = lodash.debounce((e) => {setQuery(e.target.value)},500);
+  const handleUpdateValue = lodash.debounce((e) => {
+    setQuery(e.target.value);
+  }, 500);
 
   React.useEffect(() => {
     handleFetchData.current();
   }, [query]);
 
   return (
-    <div>
+    <div className="bg-white mx-auto mt-5 p-5 rounded-lg shadow-md w-2/4">
       <input
         type="text"
-        className="border border-green-500 text-black p-5 md-5"
+        className="border border-gray-200 block w-full rounded-md p-5 mb-5 transition-all focus:border-blue-400"
+        placeholder="type your keyword..."
         defaultValue={query}
         onChange={handleUpdateValue}
       />
       {loading && (
-        <div className="loading w-8 h-8 rounded-full border-blue-500 border-4 border-r-4 border-r-transparent animate-spin">
+        <div className="loading w-8 h-8 rounded-full border-blue-500 border-4 border-r-4 border-r-transparent animate-spin mx-auto my-10">
           loading...
         </div>
       )}
-      {!loading && errorMessage && <p>{errorMessage}</p>}
-      {!loading &&
-        hits.length > 0 &&
-        hits.map((item, index) => <h3 key={item.title}>{item.title}</h3>)}
+      {!loading && errorMessage && <p className="text-red-400 my-5">{errorMessage}</p>}
+      <div className="flex flex-wrap gap-5">
+        {!loading &&
+          hits.length > 0 &&
+          hits.map((item, index) => <h3 key={item.title} className="p-3 bg-gray-100 rounded-sm">{item.title}</h3>)}
+      </div>
     </div>
   );
 };
